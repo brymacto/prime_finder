cd92b0f47222131346ec605dc571c52f6f58fbb0
Bryan MacDiarmid <bryanm@nulogy.com>
2018-09-03 16:56:24 -0400

    Extract spec assertion method and remove dummy body assertion

--- __tests__/request/prime-numbers-spec.js
+++ __tests__/request/prime-numbers-spec.js
@@ -1,19 +1,21 @@
 const request = require('supertest');
 const app = require('../../app');
 
-describe('GET /user', () => {
-  it('respond with json', (done) => {
+function expectBody(body) {
+  return (error, response) => {
+    if (error) {
+      throw new Error(error);
+    }
+    expect(response.body).toEqual(body);
+  };
+}
+
+describe('GET /prime_numbers', () => {
+  it('respond successfully with json', () => {
     request(app)
       .get('/prime_numbers')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .end((error, response) => {
-        if (error) {
-          throw new Error(error);
-        }
-        expect(response.body).toEqual({ foo: 'bar' });
-        done();
-      });
+      .expect(200);
   });
 });
