b68b923c87983a6bd6669f185f148cc9f08a6307
Bryan MacDiarmid <bryanm@nulogy.com>
2018-09-03 18:12:42 -0400

    Naively implement prime number calculator
    This is a naive implementation because it does not yet consider performance

--- __tests__/services/prime-number-calculator-spec.js
+++ __tests__/services/prime-number-calculator-spec.js
@@ -1,14 +1,6 @@
 const calculateMedianPrimeNumbers = require('../../services/prime-number-calculator');
 
 describe('#calculateMedianPrimeNumbers', () => {
-  it.only('WIP: responds with prime numbers less than upper limit', () => {
-    const upperLimit = 18;
-
-    const result = calculateMedianPrimeNumbers(upperLimit);
-
-    expect(result).toEqual([2, 3, 5, 7, 11, 13, 17]);
-  });
-
   it('responds with median of the set of prime numbers less than upper limit', () => {
     const upperLimit = 18;
 
--- services/prime-number-calculator.js
+++ services/prime-number-calculator.js
@@ -2,13 +2,14 @@ const range = require('lodash/range');
 const filter = require('lodash/filter');
 const isEqual = require('lodash/isEqual');
 const partial = require('lodash/partial');
+const sortBy = require('lodash/sortBy');
 
 function calculateMedianPrimeNumbers(upperLimit) {
   const primeNumberCandidates = range(2, upperLimit);
 
   const primeNumbers = filter(primeNumberCandidates, isPrime);
 
-  return primeNumbers;
+  return median(primeNumbers);
 
   function isPrime(primeCandidate) {
     const divisibleCandidates = range(1, primeCandidate + 1);
@@ -20,6 +21,18 @@ function calculateMedianPrimeNumbers(upperLimit) {
 
   function isDivisible(number, divisibleCandidate) {
     return (number % divisibleCandidate) === 0;
+  }
+
+  function median(numbers) {
+    const sorted = sortBy(numbers, n => n);
+
+    const isEven = isDivisible(sorted.length, 2);
+
+    if (isEven) {
+      return sorted[(sorted.length / 2)];
+    }
+
+    return sorted.slice(Math.floor(sorted.length / 2), Math.ceil(sorted.length / 2));
   }
 }
 
